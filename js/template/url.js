f4ffde5170ff515e3a81d284fd8cdd810cd1d98a
rofinafiin <rofinafiisr@gmail.com>
2023-11-02 08:27:50 +0100

    fix(masterkaryawan): merapihkan table

--- js/template/url.js
+++ js/template/url.js
@@ -2,11 +2,11 @@ export let URLKARYAWAN = "https://hris_backend.ulbi.ac.id/peg/rtm/data";
 export let URLHONORMENGAJARPRODI = "https://hris_backend.ulbi.ac.id/honor/dosen/prodi";
 
 export let templatesdm = `
-<td class="py-5 px-6 font-medium">#nama#</td>
-<td class="font-medium">#email#</td>
-<td class="font-medium">#notelp#</td>
-<td class="font-medium">#kelompok#</td>
-<td class="font-medium">#jabatan#</td>
+<td class="py-5 px-6 font-medium max-w-xs">#nama#</td>
+<td class="font-medium  max-w-xs">#email#</td>
+<td class="font-medium text-center max-w-xs">#notelp#</td>
+<td class="font-medium text-center max-w-xs">#kelompok#</td>
+<td class="font-medium text-center max-w-xs">#jabatan#</td>
 `
 export let tabletag = "tr"
 export let tableRowClass = "text-xs bg-gray-50"
