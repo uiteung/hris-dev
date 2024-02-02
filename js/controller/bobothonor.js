612b1fae977c987a701adafeac851a5eae2dc265
rofinafiin <rofinafiisr@gmail.com>
2024-02-02 14:56:08 +0700

    remove artemis assets

--- js/controller/bobothonor.js
+++
@@ -1,5 +0,0 @@
-import {get} from "https://jscroot.github.io/api/croot.js";
-import { URLEVELJABATAN } from "../template/url.js";
-import { ResponseLevel } from "../config/configbobot.js";
-
-get(URLEVELJABATAN, ResponseLevel)
