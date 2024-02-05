6cf330735a76150b30ab9e551662f87e91397002
rofinafiin <rofinafiisr@gmail.com>
2024-02-05 15:59:28 +0700

    url get history

--- static/js/controller/template.js
+++ static/js/controller/template.js
@@ -1,3 +1,4 @@
 export let GetDataValidasi = "https://hris_backend.ulbi.ac.id/api/v2/rkp/raw/"
 export let ValidasiData = "https://hris_backend.ulbi.ac.id/api/v2/rkp/validate"
 export let ValidasiHonor = "https://hris_backend.ulbi.ac.id/api/v2/rkp/rawhonor/"
+export let GetHistoryData = "https://hris_backend.ulbi.ac.id/api/v2/rkp/log/"
