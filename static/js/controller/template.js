5b8c8f0be6843b799a94ccec11acab7077986893
rofinafiin <rofinafiisr@gmail.com>
2024-02-05 12:02:01 +0700

    url honor mengajar

--- static/js/controller/template.js
+++ static/js/controller/template.js
@@ -1,2 +1,3 @@
 export let GetDataValidasi = "https://hris_backend.ulbi.ac.id/api/v2/rkp/raw/"
 export let ValidasiData = "https://hris_backend.ulbi.ac.id/api/v2/rkp/validate"
+export let ValidasiHonor = "https://hris_backend.ulbi.ac.id/api/v2/rkp/rawhonor/"
