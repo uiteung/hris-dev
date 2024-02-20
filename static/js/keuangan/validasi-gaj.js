89db844b8f3d205fd4df41688c0d5bb33ae59856
rofinafiin <rofinafiisr@gmail.com>
2024-02-20 11:52:14 +0700

    fix modal edit

--- static/js/keuangan/validasi-gaj.js
+++ static/js/keuangan/validasi-gaj.js
@@ -196,10 +196,20 @@ CihuyDomReady(() => {
                 </td>
                 <td style="text-align: center; vertical-align: middle">
                 <a href="#" class="edit" data-email-id="${email}">
-                <button type="button" class="btn btn-success">Validasi</button>
-                </a><br><br>
+                <svg class="feather feather-check" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                <polyline points="20 6 9 17 4 12"/>
+                </svg>
+                </a>
                 <a href="#" class="remove" data-email-i="${email}">
-                <button type="button" class="btn btn-danger">Batalkan</button>
+                <svg class="feather feather-x" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                <line x1="18" x2="6" y1="6" y2="18"/>
+                <line x1="6" x2="18" y1="6" y2="18"/>
+              </svg>
+              </a>
+                <a href="#" class="update" data-email-u="${email}">
+                  <svg class="feather feather-edit" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
+                  </svg
                 </a>
                 </td>
       `;
@@ -225,6 +235,37 @@ CihuyDomReady(() => {
               
               }
             });
+      } else {
+        console.error("Data gaji dengan email " + dataemail + " tidak ditemukan");
+      }
+    });
+
+
+
+    document.getElementById("tablebody").appendChild(barisBaru);
+    // Add event listeners to the buttons (outside of the loop)
+    const updatebutton = barisBaru.querySelector(".update");
+    updatebutton.addEventListener("click", () => {
+      const dataemail = updatebutton.getAttribute("data-email-u");
+      if (dataemail) {
+        Swal.fire({
+          title: "<strong>Form Update <b>Data Gaji</b></strong>",
+          html: `
+          <input type="text" id="username" class="swal2-input" placeholder=test">
+          <input type="password" id="password" class="swal2-input" placeholder="test">
+          `,
+          showCloseButton: true,
+          showCancelButton: true,
+          focusConfirm: false,
+          confirmButtonText: `
+             Confirm!
+          `,
+          confirmButtonAriaLabel: "Berhasil",
+          cancelButtonText: `
+            Cancel
+          `,
+          cancelButtonAriaLabel: "Gagal"
+        });
       } else {
         console.error("Data gaji dengan email " + dataemail + " tidak ditemukan");
       }
