6731cf6b0d013ac3a0782ff31834d94677685cc4
rofinafiin <rofinafiisr@gmail.com>
2024-02-28 14:35:35 +0700

    placeHolder form

--- static/js/controller/control.js
+++ static/js/controller/control.js
@@ -19,28 +19,45 @@ export function ModalUpdate(header, waktu) {
             const kehadiran = data.data['kehadiran'];
             const kopkar = data.data['kopkar'];
             const bankJabar = data.data['bankJabar'];
+            const arisan = data.data['arisan'];
             const bpjs = data.data['bpjs'];
             const bauk = data.data['bauk'];
             const pph = data.data['pph'];
             const lain2 = data.data['lain2'];
+
             Swal.fire({
                 title: "<strong>Form Update <b>Data Gaji</b></strong>",
-                html: `
-                <h3>Silahkan Update data yang perlu diupdate</h3>
-                <input type="text" id="gajipokok" class="swal2-input" placeholder="Gaji Pokok" value="${pokok}">
-                <input type="text" id="keluarga" class="swal2-input" placeholder="Keluarga" value="${keluarga}">
-                <input type="text" id="pangan" class="swal2-input" placeholder="Pangan" value="${pangan}">
-                <input type="text" id="keahlian" class="swal2-input" placeholder="Keahlian" value="${kinerja}">
-                <input type="text" id="fgs/struk" class="swal2-input" placeholder="FGS/Struk" value="${keahlian}">
-                <input type="text" id="transport" class="swal2-input" placeholder="Transport" value="${struk}">
-                <input type="text" id="kehadiran" class="swal2-input" placeholder="Kehadiran" value="${transportasi}">
-                <input type="text" id="kopkar" class="swal2-input" placeholder="Kopkar" value="${kehadiran}">
-                <input type="text" id="bankjabar" class="swal2-input" placeholder="Bank Jabar" value="${kopkar}">
-                <input type="text" id="arisan" class="swal2-input" placeholder="Arisan" value="${bankJabar}">
-                <input type="text" id="bpjstk" class="swal2-input" placeholder="BPJS TK" value="${bpjs}">
-                <input type="text" id="bauk" class="swal2-input" placeholder="BAUK" value="${bauk}">
-                <input type="text" id="lain-lain" class="swal2-input" placeholder="Lain - Lain" value="${lain2}">
-                <input type="text" id=pph" class="swal2-input" placeholder="pph" value="${pph}">
+                html:` 
+                <span style="text-align: left; position: relative;">Gaji Pokok</span>
+                <input type="text" id="gajipokok" class="swal2-input" placeholder="Gaji Pokok" value="${pokok}"><br><br>
+                <span style="text-align: left; position: relative;">Tunjangan Keluarga</span>
+                <input type="text" id="keluarga" class="swal2-input" placeholder="Keluarga" value="${keluarga}"><br><br>
+                <span style="text-align: left; position: relative;">Tunjangan Pangan</span>
+                <input type="text" id="pangan" class="swal2-input" placeholder="Pangan" value="${pangan}"><br><br>
+                <span style="text-align: left; position: relative;">Tunjangan Kinerja</span>
+                <input type="text" id="kinerja" class="swal2-input" placeholder="kinerja" value="${kinerja}"><br><br>
+                <span style="text-align: left; position: relative;">Tunjangan Keahlian</span>
+                <input type="text" id="keahlian" class="swal2-input" placeholder="keahlian" value="${keahlian}"><br><br>
+                <span style="text-align: left; position: relative;">Tunjangan Stuktural/Fungsional</span>
+                <input type="text" id="fgs/struk" class="swal2-input" placeholder="FGS/Struk" value="${struk}"><br><br>
+                <span style="text-align: left; position: relative;">Tunjangan Transportasi</span>
+                <input type="text" id="transport" class="swal2-input" placeholder="Transport" value="${transportasi}"><br><br>
+                <span style="text-align: left; position: relative;">Tunjangan Kehadiran</span>
+                <input type="text" id="kehadiran" class="swal2-input" placeholder="Kehadiran" value="${kehadiran}"><br><br>
+                <span style="text-align: left; position: relative;">Potongan Kopkar</span>
+                <input type="text" id="kopkar" class="swal2-input" placeholder="Kopkar" value="${kopkar}"><br><br>
+                <span style="text-align: left; position: relative;">Potongan Bank Jabar</span>
+                <input type="text" id="bankjabar" class="swal2-input" placeholder="Bank Jabar" value="${bankJabar}"><br><br>
+                <span style="text-align: left; position: relative;">Potongan Arisan</span>
+                <input type="text" id="arisan" class="swal2-input" placeholder="Arisan" value="${arisan}"><br><br>
+                <span style="text-align: left; position: relative;">Potongan BPJS</span>
+                <input type="text" id="bpjstk" class="swal2-input" placeholder="BPJS TK" value="${bpjs}"><br><br>
+                <span style="text-align: left; position: relative;">Potongan BAUK</span>
+                <input type="text" id="bauk" class="swal2-input" placeholder="BAUK" value="${bauk}"><br><br>
+                <span style="text-align: left; position: relative;">Potongan Lain - lain</span>
+                <input type="text" id="lain-lain" class="swal2-input" placeholder="Lain - Lain" value="${lain2}"><br><br>
+                <span style="text-align: left; position: relative;">Potongan PPH</span>
+                <input type="text" id=pph" class="swal2-input" placeholder="pph" value="${pph}"><br><br>
                 `,
                 showCloseButton: true,
                 showCancelButton: true,
@@ -48,7 +65,7 @@ export function ModalUpdate(header, waktu) {
                 confirmButtonText: `
                    Update
                 `,
-                confirmButtonAriaLabel: "Berhasil",
+                confirmButtonAriaLabel: data.success,
                 cancelButtonText: `
                   Cancel
                 `,
