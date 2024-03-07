49a8423b2017e7ccdfe99862b4945015e90f9bf9
rofinafiin <rofinafiisr@gmail.com>
2024-03-07 16:13:01 +0700

    fix get value data dari form search untuk gaji

--- static/js/controller/search.js
+++ static/js/controller/search.js
@@ -1,13 +1,15 @@
 import { postWithToken } from "https://jscroot.github.io/api/croot.js";
+import  { getValue } from "https://jscroot.github.io/element/croot.js";
 import { getLastMonth, responseSearch } from "./control.js";
 import { UrlSearchGaji } from "./template.js";
 import { token } from "./cookies.js";
 
 document.addEventListener("DOMContentLoaded", function() {
-  const form = document.getElementById("search");
+  const form = document.getElementById("search")
   form.addEventListener("submit", function(event) {
-    src = document.getElementById("searchinput").value
+    let src = getValue("searchinput")
+    console.log(src)
     event.preventDefault();
-    postWithToken(UrlSearchGaji + getLastMonth + '/' + src, 'login', token, "", responseSearch);
+    postWithToken(UrlSearchGaji + getLastMonth() + '/' + src, 'login', token, "", responseSearch);
   });
 });
