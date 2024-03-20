2d32c01bd6f68b30be7c1c9502b24859bd3941d2
rofinafiin <rofinafiisr@gmail.com>
2024-03-20 15:05:56 +0700

    dashboard gaji : menampilkan data setelah search

--- static/js/controller/control.js
+++ static/js/controller/control.js
@@ -1,3 +1,4 @@
+import { getBadgeMarkup } from "../style/badge.js";
 import { GetdatabyEmail, ValidasiData } from "./template.js";
 
 
@@ -191,8 +192,102 @@ export function Batal(email, header, date) {
       icon: "success"
     });
 
+    console.log(data)
+
     let inhtm = document.getElementById("tablebody")
 
     inhtm.innerHTML = "";
-    console.log(data)
+
+    const barisBaru = document.createElement("tr");
+
+      barisBaru.innerHTML= `
+              <td>
+                  <div class="d-flex align-items-center">
+                      <div class="ms-3">
+                          <p class="fw-bold mb-1">${data.data[0].nama}</p>
+                          <p class="text-muted mb-0">${data.data[0].email}</p>
+                      </div>
+                  </div>
+              </td>
+              <td style="text-align: center; vertical-align: middle">
+                <p class="fw-normal mb-1">${getLastMonth()}</p>
+              </td>
+              <td style="text-align: center; vertical-align: middle">
+                <p class="fw-normal mb-1"><b>${data.data[0].pokok}</b></p>
+              </td>
+              <td style="text-align: center; vertical-align: middle">
+                  <p class="fw-normal mb-1">${data.data[0].keluarga}</p>
+              </td>
+              <td style="text-align: center; vertical-align: middle">
+                  <p class="fw-normal mb-1">${data.data[0].pangan}</p>
+              </td>
+              <td style="text-align: center; vertical-align: middle">
+                <p class="fw-normal mb-1">${data.data[0].kinerja}</p>
+              </td>
+              <td style="text-align: center; vertical-align: middle">
+                <p class="fw-normal mb-1">${data.data[0].keahlian}</p>
+              </td>
+              <td style="text-align: center; vertical-align: middle">
+                <p class="fw-normal mb-1"><b>${data.data[0]['fgs-struk']}</b></p>
+              </td>
+              <td style="text-align: center; vertical-align: middle">
+              <p class="fw-normal mb-1">${data.data[0].transportasi}</p>
+              </td>
+                <td style="text-align: center; vertical-align: middle">
+                <p class="fw-normal mb-1">${data.data[0].kehadiran}</p>
+                </td>
+                <td style="text-align: center; vertical-align: middle">
+                <p class="fw-normal mb-1">${data.data[0].kopkar}</p>
+                </td>
+                <td style="text-align: center; vertical-align: middle">
+                <p class="fw-normal mb-1">${data.data[0].bankJabar}</p>
+                </td>
+                <td style="text-align: center; vertical-align: middle">
+                <p class="fw-normal mb-1">${data.data[0].arisan}</p>
+                </td>
+                <td style="text-align: center; vertical-align: middle">
+                <p class="fw-normal mb-1">${data.data[0].bpjs}</p>
+                </td>
+                <td style="text-align: center; vertical-align: middle">
+                <p class="fw-normal mb-1">${data.data[0].bauk}</p>
+                </td>
+                <td style="text-align: center; vertical-align: middle">
+                <p class="fw-normal mb-1">${data.data[0].lain2}</p>
+                </td>
+                <td style="text-align: center; vertical-align: middle">
+                <p class="fw-normal mb-1">${data.data[0].pph}</p>
+                </td>
+                <td style="text-align: center; vertical-align: middle">
+                <p class="fw-normal mb-1">totalgaji</p>
+                </td>
+                <td style="text-align: center; vertical-align: middle">
+                <p class="fw-normal mb-1">totalpotongan}</p>
+                </td>
+                <td style="text-align: center; vertical-align: middle">
+                <p class="fw-normal mb-1">totalgajibersih</p>
+                </td>
+                <td style="text-align: center; vertical-align: middle">
+                <p class="fw-normal mb-1">${getBadgeMarkup(data.data[0].validasi)}</p>
+                </td>
+                <td style="text-align: center; vertical-align: middle">
+                <a href="#" class="edit" data-email-id="${data.data[0].email}">
+                <svg class="feather feather-check" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                <polyline points="20 6 9 17 4 12"/>
+                </svg>
+                </a>
+                <a href="#" class="remove" data-email-i="${data.data[0].email}">
+                <svg class="feather feather-x" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                <line x1="18" x2="6" y1="6" y2="18"/>
+                <line x1="6" x2="18" y1="6" y2="18"/>
+              </svg>
+              </a>
+                <a href="#" class="update" data-email-u="${data.data[0].email}">
+                  <svg class="feather feather-edit" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
+                  </svg
+                </a>
+                </td>
+      `;
+
+      inhtm.appendChild(barisBaru)
   }
