6a7ce98b9c896fc7e87dedb61cda09c21690e462
rofinafiin <rofinafiisr@gmail.com>
2024-03-22 11:35:54 +0700

    Javascript Honor: add button validasi, cancel dan detail

--- static/js/keuangan/validasi-honor.js
+++ static/js/keuangan/validasi-honor.js
@@ -118,8 +118,25 @@ CihuyDomReady(() => {
         <td style="text-align: center; vertical-align: middle">
             <p class="fw-normal mb-1">${getBadgeMarkup(Validasi)}</p>
         </td>
+        <td style="text-align: center; vertical-align: middle">
+        <a href="#" class="edit" data-email-id="${nama}">
+        <svg class="feather feather-check" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+        <polyline points="20 6 9 17 4 12"/>
+        </svg>
+        </a>
+        <a href="#" class="remove" data-email-i="${nama}">
+        <svg class="feather feather-x" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+        <line x1="18" x2="6" y1="6" y2="18"/>
+        <line x1="6" x2="18" y1="6" y2="18"/>
+      </svg>
+      </a>
+        <a href="#" class="update" data-email-u="${nama}">
+          <svg class="feather feather-edit" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
+          </svg
+        </a>
+        </td>
       `;
-
 
 
       document.getElementById("tablebody").appendChild(barisBaru);
