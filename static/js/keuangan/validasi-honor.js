f5d75049096d130f9a90823f1d0d6bda6a2c901e
rofinafiin <rofinafiisr@gmail.com>
2024-04-06 05:25:07 +0100

    remove button detail

--- static/js/keuangan/validasi-honor.js
+++ static/js/keuangan/validasi-honor.js
@@ -131,26 +131,21 @@ CihuyDomReady(() => {
                     <line x1="6" x2="18" y1="6" y2="18"/>
                 </svg>
             </a>
-            <a href="#" class="update" data-email-u="${nama}">
-                <svg class="feather feather-edit" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
-                </svg>
-            </a>
         </td>
       `;
 
 
       document.getElementById("tablebody").appendChild(barisBaru);
-      const updatebutton = barisBaru.querySelector(".update");
-      updatebutton.addEventListener("click", () => {
-        const dataemail = updatebutton.getAttribute("data-email-u");
-        if (dataemail) {
-          header.append("email", dataemail);
-          // ModalUpdate(header, date);
-        } else {
-          console.error("Data gaji dengan email " + dataemail + " tidak ditemukan");
-        }
-      });
+      // const updatebutton = barisBaru.querySelector(".update");
+      // updatebutton.addEventListener("click", () => {
+      //   const dataemail = updatebutton.getAttribute("data-email-u");
+      //   if (dataemail) {
+      //     header.append("email", dataemail);
+      //     // ModalUpdate(header, date);
+      //   } else {
+      //     console.error("Data gaji dengan email " + dataemail + " tidak ditemukan");
+      //   }
+      // });
 
 
       const editButton = barisBaru.querySelector(".edit");
