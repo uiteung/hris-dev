910afec99ba669d49c38a993483437de83e3f3a4
rofinafiin <rofinafiisr@gmail.com>
2024-04-30 09:30:45 +0700

    Javascript: Ganti versi import sidebar

--- static/js/controller/sidebar.js
+++ static/js/controller/sidebar.js
@@ -1,4 +1,4 @@
 $(function () {
     // Load navbar.html on page load
-    $(".sidebar-nav").load("https://cdn.jsdelivr.net/gh/uiteung/hris-dev@0.0.4/static/template/sidebar.html");
+    $(".sidebar-nav").load("https://cdn.jsdelivr.net/gh/uiteung/hris-dev@0.0.5/static/template/sidebar.html");
   });
