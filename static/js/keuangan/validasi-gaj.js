4ce414eb7da2ca548f4b8677c2f804f9d78285e0
rofinafiin <rofinafiisr@gmail.com>
2024-05-02 08:45:37 +0100

    set satu bulan kebelakang

--- static/js/keuangan/validasi-gaj.js
+++ static/js/keuangan/validasi-gaj.js
@@ -31,7 +31,7 @@ CihuyDomReady(() => {
   let filteredData = []; // To store the filtered data for search
   let totalData = 0;
   // Ambil data masuk
-  fetch(GetDataValidasi  + getLastMonth(), requestOptions)
+  fetch(GetDataValidasi  + "202403", requestOptions)
     .then((result) => result.json())
     .then((rekapharian) => {
         let rkp = rekapharian.data
