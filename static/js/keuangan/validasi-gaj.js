0cd9c44506e463dce817c188ec6432d2f31062a4
christyuda <chrisyuda@ulbi.ac.id>
2024-05-03 15:17:56 +0700

    feat : merapikan tabel di js

--- static/js/keuangan/validasi-gaj.js
+++ static/js/keuangan/validasi-gaj.js
@@ -2,11 +2,16 @@
 import { CihuyId } from "https://c-craftjs.github.io/element/element.js";
 import { CihuyDomReady } from "https://c-craftjs.github.io/table/table.js";
 import { getBadgeMarkup } from "../style/badge.js";
-import { GetDataValidasi, ValidasiData} from "../controller/template.js";
+import { GetDataValidasi, ValidasiData } from "../controller/template.js";
 
 // Untuk Autentifikasi Login User Tertentu
 import { token } from "../controller/cookies.js";
-import { Batal, ModalUpdate, getLastMonth, validate } from "../controller/control.js";
+import {
+  Batal,
+  ModalUpdate,
+  getLastMonth,
+  validate,
+} from "../controller/control.js";
 
 var header = new Headers();
 header.append("login", token);
@@ -14,57 +19,57 @@ header.append("Content-Type", "application/json");
 
 const requestOptions = {
   method: "GET",
-  headers: header
+  headers: header,
 };
-  
+
 // Call the function to get the last month
 const date = getLastMonth();
 
 // Untuk Get All data Presensi
 CihuyDomReady(() => {
-//   const tablebody = CihuyId("tablebody");
+  //   const tablebody = CihuyId("tablebody");
   const buttonsebelumnya = CihuyId("prevPageBtn");
   const buttonselanjutnya = CihuyId("nextPageBtn");
   const halamansaatini = CihuyId("currentPage");
-  const itemperpage =6;
-  let halamannow = localStorage.getItem('currentPage') || 1;
+  const itemperpage = 6;
+  let halamannow = localStorage.getItem("currentPage") || 1;
   let filteredData = []; // To store the filtered data for search
   let totalData = 0;
   // Ambil data masuk
-  fetch(GetDataValidasi  + "202403", requestOptions)
+  fetch(GetDataValidasi + "202403", requestOptions)
     .then((result) => result.json())
     .then((rekapharian) => {
-        let rkp = rekapharian.data
+      let rkp = rekapharian.data;
 
-          // Inisialisasi data tabel
-          totalData = rkp.length;
-          filteredData = rkp;
+      // Inisialisasi data tabel
+      totalData = rkp.length;
+      filteredData = rkp;
 
-          // Untuk Memunculkan Pagination Halamannya
+      // Untuk Memunculkan Pagination Halamannya
+      displayData(halamannow);
+      updatePagination();
+
+      // Button Pagination (Sebelumnya)
+      buttonsebelumnya.addEventListener("click", () => {
+        if (halamannow > 1) {
+          halamannow--;
           displayData(halamannow);
           updatePagination();
+          localStorage.setItem("currentPage", halamannow);
+        }
+      });
 
-          // Button Pagination (Sebelumnya)
-          buttonsebelumnya.addEventListener("click", () => {
-            if (halamannow > 1) {
-              halamannow--;
-              displayData(halamannow);
-              updatePagination();
-              localStorage.setItem('currentPage', halamannow);
-            }
-          });
-
-          // Button Pagination (Selanjutnya)
-          buttonselanjutnya.addEventListener("click", () => {
-            const totalPages = Math.ceil(totalData / itemperpage);
-            if (halamannow < totalPages) {
-              halamannow++;
-              displayData(halamannow);
-              updatePagination();
-              localStorage.setItem('currentPage', halamannow);
-            }
-          })
-        //   console.log(rekapharian)
+      // Button Pagination (Selanjutnya)
+      buttonselanjutnya.addEventListener("click", () => {
+        const totalPages = Math.ceil(totalData / itemperpage);
+        if (halamannow < totalPages) {
+          halamannow++;
+          displayData(halamannow);
+          updatePagination();
+          localStorage.setItem("currentPage", halamannow);
+        }
+      });
+      //   console.log(rekapharian)
     });
 
   // Fungsi Untuk Menampilkan Data
@@ -78,40 +83,39 @@ CihuyDomReady(() => {
 
     // Iterasi melalui rowsToShow dan membangun baris tabel
     rowsToShow.forEach((combinedEntry) => {
-
       // Ekstrak data yang relevan
       const nama = combinedEntry.nama;
       const email = combinedEntry.email;
       const waktu = date;
-      const pokok = combinedEntry['gaji-pokok'];
-      const keluarga = combinedEntry['keluarga'];
-      const pangan = combinedEntry['pangan'];
-      const kinerja = combinedEntry['kinerja'];
-      const keahlian = combinedEntry['keahlian'];
-      const struk = combinedEntry['struk'];
-      const transportasi = combinedEntry['transportasi'];
-      const kehadiran = combinedEntry['kehadiran'];
-      const kopkar = combinedEntry['kopkar'];
-      const bankJabar = combinedEntry['bankJabar'];
-      const bpjs = combinedEntry['bpjs'];
-      const bauk = combinedEntry['bauk'];
-      const pph = combinedEntry['pph'];
-      const lain2 = combinedEntry['lain2'];
-      const arisan = combinedEntry['arisan'];
-      const totalgaji = combinedEntry['totalgaji'];
-      const totalgajibersih = combinedEntry['totalgajibersih'];
-      const totalpotongan = combinedEntry['totalpotongan'];
-      const statusvalidasi = combinedEntry['validasi'];
-    //   console.log(struk)
+      const pokok = combinedEntry["gaji-pokok"];
+      const keluarga = combinedEntry["keluarga"];
+      const pangan = combinedEntry["pangan"];
+      const kinerja = combinedEntry["kinerja"];
+      const keahlian = combinedEntry["keahlian"];
+      const struk = combinedEntry["struk"];
+      const transportasi = combinedEntry["transportasi"];
+      const kehadiran = combinedEntry["kehadiran"];
+      const kopkar = combinedEntry["kopkar"];
+      const bankJabar = combinedEntry["bankJabar"];
+      const bpjs = combinedEntry["bpjs"];
+      const bauk = combinedEntry["bauk"];
+      const pph = combinedEntry["pph"];
+      const lain2 = combinedEntry["lain2"];
+      const arisan = combinedEntry["arisan"];
+      const totalgaji = combinedEntry["totalgaji"];
+      const totalgajibersih = combinedEntry["totalgajibersih"];
+      const totalpotongan = combinedEntry["totalpotongan"];
+      const statusvalidasi = combinedEntry["validasi"];
+      //   console.log(struk)
 
       const barisBaru = document.createElement("tr");
 
-      barisBaru.innerHTML= `
-              <td>
+      barisBaru.innerHTML = `
+              <td style="margin: px">
                   <div class="d-flex align-items-center">
                       <div class="ms-3">
-                          <p class="fw-bold mb-1">${nama}</p>
-                          <p class="text-muted mb-0">${email}</p>
+                          <p class="fw-bold mb-3 text-left" style="font-size: 12px;  white-space: pre-line;">${nama}</p>
+                          <p class="text-muted text-left " style="font-size: 12px;  white-space: pre-line;">${email}</p>
                       </div>
                   </div>
               </td>
@@ -196,73 +200,75 @@ CihuyDomReady(() => {
       `;
 
       document.getElementById("tablebody").appendChild(barisBaru);
-    // Add event listeners to the buttons (outside of the loop)
-    const editButton = barisBaru.querySelector(".edit");
-    editButton.addEventListener("click", () => {
-      const dataemail = editButton.getAttribute("data-email-id");
-      if (dataemail) {
+      // Add event listeners to the buttons (outside of the loop)
+      const editButton = barisBaru.querySelector(".edit");
+      editButton.addEventListener("click", () => {
+        const dataemail = editButton.getAttribute("data-email-id");
+        if (dataemail) {
           Swal.fire({
-              title: "Validasi Data Gaji?",
-              text: "Apakah Anda yakin ingin menvalidasi data ini?",
-              icon: "question",
-              showCancelButton: true,
-              confirmButtonText: "Ya, Update",
-              cancelButtonText: "Batal",
-            }).then((result) => {
-              if (result.isConfirmed) {
-                // Kirim permintaan PUT/UPDATE ke server tanpa gambar
-                validate(dataemail, header, date);   
-                localStorage.setItem('currentPage', halamannow);
-              
-              }
-            });
-      } else {
-        console.error("Data gaji dengan email " + dataemail + " tidak ditemukan");
-      }
-    });
+            title: "Validasi Data Gaji?",
+            text: "Apakah Anda yakin ingin menvalidasi data ini?",
+            icon: "question",
+            showCancelButton: true,
+            confirmButtonText: "Ya, Update",
+            cancelButtonText: "Batal",
+          }).then((result) => {
+            if (result.isConfirmed) {
+              // Kirim permintaan PUT/UPDATE ke server tanpa gambar
+              validate(dataemail, header, date);
+              localStorage.setItem("currentPage", halamannow);
+            }
+          });
+        } else {
+          console.error(
+            "Data gaji dengan email " + dataemail + " tidak ditemukan"
+          );
+        }
+      });
 
+      document.getElementById("tablebody").appendChild(barisBaru);
+      // Add event listeners to the buttons (outside of the loop)
+      const updatebutton = barisBaru.querySelector(".update");
+      updatebutton.addEventListener("click", () => {
+        const dataemail = updatebutton.getAttribute("data-email-u");
+        if (dataemail) {
+          header.append("email", dataemail);
+          ModalUpdate(header, date);
+        } else {
+          console.error(
+            "Data gaji dengan email " + dataemail + " tidak ditemukan"
+          );
+        }
+      });
 
-
-    document.getElementById("tablebody").appendChild(barisBaru);
-    // Add event listeners to the buttons (outside of the loop)
-    const updatebutton = barisBaru.querySelector(".update");
-    updatebutton.addEventListener("click", () => {
-      const dataemail = updatebutton.getAttribute("data-email-u");
-      if (dataemail) {
-        header.append("email", dataemail);
-        ModalUpdate(header, date);
-      } else {
-        console.error("Data gaji dengan email " + dataemail + " tidak ditemukan");
-      }
-    });
-
-
-    const batalbutton = barisBaru.querySelector(".remove");
-    batalbutton.addEventListener("click", () => {
-      const dataemail = batalbutton.getAttribute("data-email-i");
-      if (dataemail) {
+      const batalbutton = barisBaru.querySelector(".remove");
+      batalbutton.addEventListener("click", () => {
+        const dataemail = batalbutton.getAttribute("data-email-i");
+        if (dataemail) {
           Swal.fire({
-              title: "Batal Validasi?",
-              text: "Apakah Anda yakin ingin Membatalkan Validasi?",
-              icon: "question",
-              showCancelButton: true,
-              confirmButtonText: "Ya",
-              cancelButtonText: "Batal",
-            }).then((result) => {
-              if (result.isConfirmed) {
-                // Kirim permintaan PUT/UPDATE ke server tanpa gambar
-                Batal(dataemail, header, date); 
-                localStorage.setItem('currentPage', halamannow);
-             }
-            });
-      } else {
-        console.error("Data gaji dengan email " + dataemail + " tidak ditemukan");
-      }
+            title: "Batal Validasi?",
+            text: "Apakah Anda yakin ingin Membatalkan Validasi?",
+            icon: "question",
+            showCancelButton: true,
+            confirmButtonText: "Ya",
+            cancelButtonText: "Batal",
+          }).then((result) => {
+            if (result.isConfirmed) {
+              // Kirim permintaan PUT/UPDATE ke server tanpa gambar
+              Batal(dataemail, header, date);
+              localStorage.setItem("currentPage", halamannow);
+            }
+          });
+        } else {
+          console.error(
+            "Data gaji dengan email " + dataemail + " tidak ditemukan"
+          );
+        }
+      });
     });
-  });
 
     // document.getElementById("tablebody").innerHTML = tableData;
-    updatePagination()
+    updatePagination();
   }
 
   // Fungsi Untuk Update Pagination
