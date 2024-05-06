8703baafa4c7519ac618a04f9c5c4d59a01a8a1f
christyuda <chrisyuda@ulbi.ac.id>
2024-05-06 13:55:19 +0700

    feat : update sidebar

--- static/js/controller/sidebar.js
+++ static/js/controller/sidebar.js
@@ -1,4 +1,6 @@
 $(function () {
-    // Load navbar.html on page load
-    $(".sidebar-nav").load("https://cdn.jsdelivr.net/gh/uiteung/hris-dev@0.0.5/static/template/sidebar.html");
-  });
+  // Load navbar.html on page load
+  $(".sidebar-nav").load(
+    "https://cdn.jsdelivr.net/gh/uiteung/hris-dev@0.0.7/static/template/sidebar.html"
+  );
+});
