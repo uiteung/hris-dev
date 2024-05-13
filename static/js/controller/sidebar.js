84f691fcb5618332308682c5efcdd7a03a7c4841
rofinafiin <rofinafiisr@gmail.com>
2024-05-13 16:38:33 +0700

    sidebar javascript

--- static/js/controller/sidebar.js
+++ static/js/controller/sidebar.js
@@ -1,6 +1,6 @@
 $(function () {
   // Load navbar.html on page load
   $(".sidebar-nav").load(
-    "https://cdn.jsdelivr.net/gh/uiteung/hris-dev@0.1.4/static/template/sidebar.html"
+    "https://cdn.jsdelivr.net/gh/uiteung/hris-dev@0.1.5/static/template/sidebar.html"
   );
 });
