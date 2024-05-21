44812e904a43525c82f2fcdd80582b3084f10ccf
christyuda <chrisyuda@ulbi.ac.id>
2024-05-21 13:13:03 +0700

    feat : berhasil menambahkan hapus dan delete

--- static/js/keuangan/doktor/doktor-add.js
+++ static/js/keuangan/doktor/doktor-add.js
@@ -2,28 +2,30 @@ import { token } from "../../controller/cookies.js";
 function postData() {
   const url = "https://hris_backend.ulbi.ac.id/api/v2/master/doktor/insert";
 
-  const nama = document.getElementById("nama");
-  const email = document.getElementById("email");
-  const jafung = document.getElementById("jafung");
-  const jabatan_struktural = document.getElementById("jabatan_struktural");
-  const tunjangan = document.getElementById("tunjangan");
-  const pph = document.getElementById("pph");
-  const jumlah_dibayarkan = document.getElementById("jumlah_dibayarkan");
-  const masa_perolehan = document.getElementById("masa_perolehan");
-  const validasi = document.getElementById("validasi");
+  const nama = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const jafung = document.getElementById("jafung").value;
+  const jabatan_struktural =
+    document.getElementById("jabatan_struktural").value;
+  const tunjangan = parseFloat(document.getElementById("tunjangan").value);
+  const pph = parseFloat(document.getElementById("pph").value);
+  const jumlah_dibayarkan = parseFloat(
+    document.getElementById("jumlah_dibayarkan").value
+  );
+  const masa_perolehan = document.getElementById("masa_perolehan").value;
+  const validasi = document.getElementById("validasi").value;
 
   const data = {
     nama: nama,
     email: email,
-    jafung: jafung,
+    jabatan_fungsional: jafung,
     jabatan_struktural: jabatan_struktural,
-    tunjangan: parseInt(tunjangan),
-    pph: parseInt(pph),
-    jumlah_dibayarkan: parseInt(jumlah_dibayarkan),
+    tunjangan: tunjangan,
+    pph: pph,
+    jumlah_dibayarkan: jumlah_dibayarkan,
     masa_perolehan: masa_perolehan,
-    validasi: validasi,
+    validasi: validasi === "true", // Convert to boolean
   };
-
   const options = {
     method: "POST",
     headers: {
@@ -47,9 +49,16 @@ function postData() {
         .then((response) => response.json())
         .then((data) => {
           console.log("Success:", data);
-          Swal.fire("Submitted!", "Your data has been submitted.", "success");
-          window.location.href =
-            "https://euis.ulbi.ac.id/hris-dev/app/Doktor/doktor-master.html";
+          Swal.fire(
+            "Submitted!",
+            "Your data has been submitted.",
+            "success"
+          ).then(() => {
+            location.reload(true);
+          });
+
+          //   window.location.href =
+          //     "https://euis.ulbi.ac.id/hris-dev/app/Doktor/doktor-master.html";
         })
         .catch((error) => {
           console.error("Error:", error);
