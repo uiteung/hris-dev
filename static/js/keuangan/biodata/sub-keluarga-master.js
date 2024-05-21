86794d38a94089ee0692e4c7181b0a77b443dd35
christyuda <chrisyuda@ulbi.ac.id>
2024-05-21 10:06:17 +0700

    feat : buat add data sub keluarga

--- static/js/keuangan/biodata/sub-keluarga-master.js
+++ static/js/keuangan/biodata/sub-keluarga-master.js
@@ -389,3 +389,56 @@ function sendDeleteRequest(email) {
       Swal.fire("Error", "Kesalahan: " + error.message, "error");
     });
 }
+
+function postData() {
+  const url =
+    "https://hris_backend.ulbi.ac.id/api/v2/master/komponenkeluarga/insert";
+
+  const jenis = document.getElementById("jenis");
+  const nominal = parseInt(document.getElementById("nominal"));
+  const persentase = parseInt(document.getElementById("persentase"));
+
+  const data = {
+    jenis: jenis,
+    nominal: nominal,
+    persentase: persentase,
+  };
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      login: token,
+    },
+    body: JSON.stringify(data),
+  };
+  Swal.fire({
+    title: "Anda Yakin?",
+    text: "Kamu ingin mengirim data!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, submit it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      // Only proceed with fetch if user confirms
+      fetch(url, options)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Success:", data);
+          Swal.fire("Submitted!", "Your data has been submitted.", "success");
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          Swal.fire(
+            "Failed!",
+            "There was an issue submitting your data.",
+            "error"
+          );
+        });
+    }
+  });
+}
+
+document.getElementById("updatebutton").addEventListener("click", postData);
