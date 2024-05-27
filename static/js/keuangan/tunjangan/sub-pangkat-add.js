c9e2d78c6f10e8d441a7c3e193e9934d91a028df
christyuda <chrisyuda@ulbi.ac.id>
2024-05-27 08:54:08 +0700

    feat : mengganti swal fire ketika menginput data

--- static/js/keuangan/tunjangan/sub-pangkat-add.js
+++ static/js/keuangan/tunjangan/sub-pangkat-add.js
@@ -21,8 +21,8 @@ function postData() {
     body: JSON.stringify(data),
   };
   Swal.fire({
-    title: "Are you sure?",
-    text: "You won't be able to revert this!",
+    title: "Anda Yakin?",
+    text: "Kamu ingin mengirim data!",
     icon: "warning",
     showCancelButton: true,
     confirmButtonColor: "#3085d6",
