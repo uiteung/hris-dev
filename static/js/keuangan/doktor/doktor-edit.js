6d8f4c737d1dbcd1bd5c6b4ae4b3af8910ebe291
christyuda <chrisyuda@ulbi.ac.id>
2024-05-30 14:11:23 +0700

    feat : tambah doktor edit js

--- static/js/keuangan/doktor/doktor-edit.js
+++ static/js/keuangan/doktor/doktor-edit.js
@@ -47,11 +47,46 @@ function fetchUserDataByEmail(email) {
 function populateForm(userData) {
   document.getElementById("nama").value = userData.nama || "";
   document.getElementById("email").value = userData.email || "";
+  document.getElementById("jafung").value = userData.jabatan_fungsional || "";
+  document.getElementById("jabatan_struktural").value =
+    userData.jabatan_struktural || "";
+
+  document.getElementById("pph").value = userData.pph || "";
+  document.getElementById("jumlah_dibayarkan").value =
+    userData.jumlah_dibayarkan || "";
+
+  document.getElementById("tunjangan").value = userData.tunjangan || "";
+  document.getElementById("validasi").value = userData.validasi || "";
+
+  if (userData.masa_perolehan) {
+    const masaPerolehan = document.getElementById("masa_perolehan");
+    const formattedMasaPerolehan = capitalize(
+      userData.masa_perolehan.toLowerCase()
+    );
+    selectDropdownOption(masaPerolehan, formattedMasaPerolehan);
+  }
+}
+
+function capitalize(text) {
+  return text
+    .split(" ")
+    .map((word) => word.toUpperCase())
+    .join(" ");
+}
+
+// Function to select the correct dropdown option
+function selectDropdownOption(dropdown, valueToSelect) {
+  for (const option of dropdown.options) {
+    if (option.value === valueToSelect) {
+      option.selected = true;
+      break;
+    }
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("editForm");
-  form.addEventListener("submit", function (event) {
+  const buttonupdate = document.getElementById("updatebutton");
+  buttonupdate.addEventListener("click", function (event) {
     event.preventDefault();
     Swal.fire({
       title: "Konfirmasi",
@@ -72,13 +107,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function updateUserData() {
   const email = document.getElementById("email").value; // mendapatkan email dari form
-  const url = `https://hris_backend.ulbi.ac.id/api/v2/wagemst/update?email=${encodeURIComponent(
+  const url = `https://hris_backend.ulbi.ac.id/api/v2/master/doktor/edit?email=${encodeURIComponent(
     email
   )}`;
 
   const formData = {
     nama: document.getElementById("nama").value,
     email: document.getElementById("email").value,
+    jabatan_fungsional: document.getElementById("jafung").value,
+    jabatan_struktural: document.getElementById("jabatan_struktural").value,
+    jumlah_dibayarkan: parseFloat(
+      document.getElementById("jumlah_dibayarkan").value
+    ),
+    pph: parseFloat(document.getElementById("pph").value),
+    tunjangan: parseFloat(document.getElementById("tunjangan").value),
+    validasi: document.getElementById("validasi").value,
+    masa_perolehan: document.getElementById("masa_perolehan").value,
   };
 
   fetch(url, {
@@ -101,7 +145,7 @@ function updateUserData() {
           setTimeout(
             () =>
               (window.location.href =
-                "https://euis.ulbi.ac.id/hris-dev/app/master-data.html"),
+                "https://euis.ulbi.ac.id/hris-dev/app/Doktor/dokter-master.html"),
             1000
           );
         });
