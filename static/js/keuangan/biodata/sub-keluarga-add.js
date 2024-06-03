bad0a3ba92383843d3613602c9953f848d2a803d
christyuda <chrisyuda@ulbi.ac.id>
2024-06-03 09:41:16 +0700

    feat : ganti nominal form untuk form rupiah

--- static/js/keuangan/biodata/sub-keluarga-add.js
+++ static/js/keuangan/biodata/sub-keluarga-add.js
@@ -1,11 +1,33 @@
 import { token } from "../../controller/cookies.js";
-
+function formatAsRupiah(input) {
+  let value = input.value;
+  // Remove all characters except digits and comma
+  value = value.replace(/[^0-9]/g, "");
+  // Convert string to an integer
+  let number = parseInt(value, 10);
+  if (isNaN(number)) {
+    number = 0;
+  }
+  // Format the number with Indonesian locale to add thousand separators
+  value = number.toLocaleString("id-ID");
+  // Prepend 'Rp' and a space to denote Rupiah
+  input.value = `Rp ${value}`;
+}
+document.addEventListener("DOMContentLoaded", function () {
+  const currencyFields = ["nominal"];
+  currencyFields.forEach((fieldId) => {
+    const inputField = document.getElementById(fieldId);
+    inputField.addEventListener("input", () => formatAsRupiah(inputField));
+  });
+});
 function postData() {
   const url =
     "https://hris_backend.ulbi.ac.id/api/v2/master/komponenkeluarga/insert";
 
   const jenis = document.getElementById("jenis").value;
-  const nominal = parseFloat(document.getElementById("nominal").value);
+  const nominal = parseFloat(
+    document.getElementById("nominal").value.replace(/[^0-9]/g, "")
+  );
   const persentase = parseFloat(document.getElementById("persentase").value);
 
   const data = {
