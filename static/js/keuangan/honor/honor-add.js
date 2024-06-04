3a67c84663d39e646c01fb271d8cf8d6e9db1d2a
christyuda <chrisyuda@ulbi.ac.id>
2024-06-04 15:05:00 +0700

    feat : ganti honor url di honor add

--- static/js/keuangan/honor/honor-add.js
+++ static/js/keuangan/honor/honor-add.js
@@ -2,14 +2,11 @@ import { token } from "../../controller/cookies.js";
 
 function postData() {
   const url =
-    "https://hris_backend.ulbi.ac.id/api/v2/honor/honormengajar/insert";
+    "https://hris_backend.ulbi.ac.id/api/v2/honour/honormengajar/insert";
 
   const nama_pengajar = document.getElementById("name").value;
   const phone_number = document.getElementById("phoneNumber").value;
-  const total_honor = parseFloat(document.getElementById("total_honor").value);
-  const jabatan = document.getElementById("jabatan").value;
 
-  const pph = parseFloat(document.getElementById("pph").value);
   const mata_kuliah = Array.from(
     document.getElementsByClassName("courseItem")
   ).map((course) => ({
@@ -18,24 +15,17 @@ function postData() {
     kelas: course.querySelector('[name="kelas[]"]').value,
     jam: parseFloat(course.querySelector('[name="jam[]"]').value),
     maks_kjm: parseFloat(course.querySelector('[name="maks_kjm[]"]').value),
-    jumlah_kelas: course.querySelector('[name="jumlah_kelas[]"]').value,
+    jumlah_kelas: parseInt(course.querySelector('[name="jumlah_kelas[]"]'))
+      .value,
     jumlah_temu: parseFloat(
       course.querySelector('[name="jumlah_temu[]"]').value
     ),
-    jam_dibayar: parseFloat(
-      course.querySelector('[name="jam_dibayar[]"]').value
-    ),
-    honor_ajar: parseFloat(course.querySelector('[name="honor_ajar[]"]').value),
   }));
 
   const data = {
     nama_pengajar: nama_pengajar,
     phone_number: phone_number,
     mata_kuliah: mata_kuliah,
-    total_honor: total_honor,
-    jabatan: jabatan,
-    pph: pph,
-    jumlah_dibayar: 1, // Assuming a constant value or retrieve from another input if needed
   };
 
   const options = {
@@ -69,7 +59,7 @@ function postData() {
             setTimeout(
               () =>
                 (window.location.href =
-                  "https://euis.ulbi.ac.id/hris-dev/app/Honor/honor-master.html"),
+                  "https://euis.ulbi.ac.id/hris-dev/app/honor/honor-master.html"),
               1000
             );
           });
