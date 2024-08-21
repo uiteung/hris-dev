88e68321790ad34fe5059d92e5e20faed4a47584
rofinafiin <rofinafiisr@gmail.com>
2024-08-21 12:00:46 +0700

    function convert keterangan

--- static/js/keuangan/prodi/utils.js
+++ static/js/keuangan/prodi/utils.js
@@ -8,3 +8,18 @@ export function ConvertJenisPrimbon(jenis) {
     }
     return jenis
 }
+
+export function ConvertKeteranganPrimbon(keterangan) {
+    if (keterangan == "pu") {
+        keterangan = "Pendamping Utama"
+    } else if (keterangan == "pp") {
+        keterangan = "Pembimbing Pendamping"
+    } else if (keterangan == "pb") {
+        keterangan = "Pembimbing"
+    } else if (keterangan == "pb1") {
+        keterangan = "Pembimbing 1"
+    } else if (keterangan == "pb2") {
+        keterangan = "Pembimbing 2"
+    }
+    return keterangan
+}
