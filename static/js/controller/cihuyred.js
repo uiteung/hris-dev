7dc31638b20fd66e2b5200536cbe692015d13f75
KostsPlayer <azka.nuril070@gmail.com>
2025-01-03 18:36:36 +0700

    update logout URL to point to the HRIS authentication page

--- static/js/controller/cihuyred.js
+++ static/js/controller/cihuyred.js
@@ -2,6 +2,6 @@ import {
     CihuyTokRed,
   } from "https://c-craftjs.github.io/link/link.js";
   
-  const redirecturl = "https://euis.ulbi.ac.id";
+  const redirecturl = "https://euis.ulbi.ac.id/hris-auth/";
   const token = "login";
   CihuyTokRed(token, redirecturl);
