804484e5b093074021fb57a872e5f112a6c58356
TLH DTI <tlhdti@ulbi.ac.id>
2025-01-03 12:57:59 +0700

    validation role for sidebar and table

--- static/js/style/table-sorting.js
+++ static/js/style/table-sorting.js
@@ -1,71 +1,478 @@
+const SECRET_KEY = "!uLBi123!"; // Pastikan kunci sama dengan yang digunakan untuk enkripsi
+
+function decryptRole() {
+  const encryptedRole = getCookieValue("hris-role");
+  if (!encryptedRole) {
+    console.error("No encrypted role found in cookies.");
+    return null;
+  }
+
+  try {
+    // Dekripsi nilai dari cookie
+    const bytes = CryptoJS.AES.decrypt(encryptedRole, SECRET_KEY);
+    const decryptedRole = bytes.toString(CryptoJS.enc.Utf8);
+
+    // Kembalikan role yang didekripsi
+    return decryptedRole;
+  } catch (error) {
+    console.error("Failed to decrypt role:", error);
+    return null;
+  }
+}
+
+// Fungsi untuk membaca cookie
+function getCookieValue(cookieName) {
+  const cookies = document.cookie.split("; ");
+  const cookie = cookies.find((row) => row.startsWith(`${cookieName}=`));
+  return cookie ? cookie.split("=")[1] : null;
+}
+
+// Dekripsi dan cetak hasilnya
+const role = decryptRole();
+
+const sidebarMenus = document.getElementById("sidebar-menus");
+const tableHeader = document.getElementById("example");
+
+console.log(role);
+
+if (role === "DTI" || role === "keuangan") {
+  sidebarMenus.innerHTML = `
+   <li class="sidebar-header">
+		Master
+		<i
+		  class="fa fa-fw fa-check-square-o"
+		  style="
+			background-color: #ffffff;
+			color: rgb(255, 0, 0);
+			padding: 2px 5px;
+			border-radius: 5px;
+			font-size: 6.5px;
+		  "
+		  >On Progress</i
+		>
+	  </li>
+	  <li class="sidebar-item">
+		<a
+		  class="sidebar-link"
+		  href="https://euis.ulbi.ac.id/hris-dev/app/Biodata/biodata-master.html"
+		>
+		  <i class="align-middle" data-feather="user"></i>
+		  <span class="align-middle">Master Biodata</span>
+		</a>
+		<ul class="sub-menu">
+		  <li class="sidebar-item">
+			<a
+			  class="sidebar-link"
+			  href="https://euis.ulbi.ac.id/hris-dev/app/Biodata/keluarga-master.html"
+			>
+			  <i class="align-middle" data-feather="users"></i>
+			  <span class="align-middle">Komponen Keluarga</span>
+			</a>
+		  </li>
+		  <li class="sidebar-item">
+			<a
+			  class="sidebar-link"
+			  href="https://euis.ulbi.ac.id/hris-dev/app/Biodata/pangkat-master.html"
+			>
+			  <i class="align-middle" data-feather="user-check"></i>
+			  <span class="align-middle">Pangkat/Golongan</span>
+			</a>
+		  </li>
+		  <li class="sidebar-item">
+			<a
+			  class="sidebar-link"
+			  href="https://euis.ulbi.ac.id/hris-dev/app/Biodata/jabatan-fungsional-master.html"
+			>
+			  <i class="align-middle" data-feather="award"></i>
+			  <span class="align-middle">Jabatan Fungsional</span>
+			</a>
+		  </li>
+		  <li class="sidebar-item">
+			<a
+			  class="sidebar-link"
+			  href="https://euis.ulbi.ac.id/hris-dev/app/dikjar/dikjar-master.html"
+			>
+			  <i class="align-middle" data-feather="book"></i>
+			  <span class="align-middle">Maksimal Mengajar</span>
+			</a>
+		  </li>
+		</ul>
+	  </li>
+	  <li class="sidebar-item">
+		<a
+		  class="sidebar-link"
+		  href="https://euis.ulbi.ac.id/hris-dev/app/Tunjangan/tunjangan-master.html"
+		>
+		  <i class="align-middle" data-feather="activity"></i>
+		  <span class="align-middle">Master Tunjangan</span>
+		</a>
+	  </li>
+	  <li class="sidebar-item">
+		<a
+		  class="sidebar-link"
+		  href="https://euis.ulbi.ac.id/hris-dev/app/Potongan/potongan-master.html"
+		>
+		  <i class="align-middle" data-feather="credit-card"></i>
+		  <span class="align-middle">Master Potongan</span>
+		</a>
+		<ul class="sub-menu">
+		  <li class="sidebar-item">
+			<a
+			  class="sidebar-link"
+			  href="https://euis.ulbi.ac.id/hris-dev/app/Biodata/pph-master.html"
+			>
+			  <i class="align-middle" data-feather="dollar-sign"></i>
+			  <span class="align-middle">Master Pph</span>
+			</a>
+		  </li>
+		</ul>
+	  </li>
+	  <li class="sidebar-item">
+		<a
+		  class="sidebar-link"
+		  href="https://euis.ulbi.ac.id/hris-dev/app/Doktor/doktor-master.html"
+		>
+		  <i class="align-middle" data-feather="check"></i>
+		  <span class="align-middle">Master Doktor</span>
+		</a>
+	  </li>
+	  <li class="sidebar-header">
+		Penggajian
+		<i
+		  class="fa fa-fw fa-check-square-o"
+		  style="
+			background-color: #ffffff;
+			color: rgb(255, 0, 0);
+			padding: 2px 5px;
+			border-radius: 5px;
+			font-size: 6.5px;
+		  "
+		  >Testing BETA</i
+		>
+	  </li>
+	  <li class="sidebar-item">
+		<a
+		  class="sidebar-link"
+		  href="https://euis.ulbi.ac.id/hris-dev/app/master-data.html"
+		>
+		  <i class="align-middle" data-feather="bar-chart-2"></i>
+		  <span class="align-middle">Detail Gaji</span>
+		</a>
+	  </li>
+	  <li class="sidebar-item">
+		<a
+		  class="sidebar-link"
+		  href="https://euis.ulbi.ac.id/hris-dev/app/validasi-data.html"
+		>
+		  <i class="align-middle" data-feather="check"></i>
+		  <span class="align-middle">Dashboard Validasi Data</span>
+		</a>
+	  </li>
+	  <li class="sidebar-item">
+		<a
+		  class="sidebar-link"
+		  href="https://euis.ulbi.ac.id/hris-dev/app/history-data.html"
+		>
+		  <i class="align-middle" data-feather="file-text"></i>
+		  <span class="align-middle">History Slip Gaji</span>
+		</a>
+	  </li>
+	  <li class="sidebar-item">
+		<a
+		  class="sidebar-link"
+		  href="https://euis.ulbi.ac.id/hris-dev/app/history-slip-gaji-bulanan.html"
+		>
+		  <i class="align-middle" data-feather="file-text"></i>
+		  <span class="align-middle">History Slip Gaji Perbulan</span>
+		</a>
+	  </li>
+	  <li class="sidebar-header">
+		Honor Ajar
+		<i
+		  class="fa fa-fw fa-check-square-o"
+		  style="
+			background-color: #ffffff;
+			color: rgb(255, 0, 0);
+			padding: 2px 5px;
+			border-radius: 5px;
+			font-size: 6.5px;
+		  "
+		  >On Progress</i
+		>
+	  </li>
+	  <li class="sidebar-item">
+		<a
+		  class="sidebar-link"
+		  href="https://euis.ulbi.ac.id/hris-dev/app/honor/honor-master.html"
+		>
+		  <i class="align-middle" data-feather="bar-chart-2"></i>
+		  <span class="align-middle">Detail Honor Ajar</span>
+		</a>
+	  </li>
+	  <li class="sidebar-item">
+		<a
+		  class="sidebar-link"
+		  href="https://euis.ulbi.ac.id/hris-dev/app/honor/honor-validasi.html"
+		>
+		  <i class="align-middle" data-feather="check"></i>
+		  <span class="align-middle">Dashboard Validasi Data</span>
+		</a>
+	  </li>
+	  <li class="sidebar-item">
+		<a
+		  class="sidebar-link"
+		  href="https://euis.ulbi.ac.id/hris-dev/app/honor/honor-histori.html"
+		>
+		  <i class="align-middle" data-feather="file-text"></i>
+		  <span class="align-middle">History Slip Honor Ajar</span>
+		</a>
+	  </li>
+	  <li class="sidebar-header">
+		Honor Assessment
+		<i
+		  class="fa fa-fw fa-check-square-o"
+		  style="
+			background-color: #ffffff;
+			color: rgb(255, 0, 0);
+			padding: 2px 5px;
+			border-radius: 5px;
+			font-size: 6.5px;
+		  "
+		  >On Progress</i
+		>
+	  </li>
+	  <li class="sidebar-item">
+		<a
+		  class="sidebar-link"
+		  href="https://euis.ulbi.ac.id/hris-dev/app/honor/honor-assessment.html"
+		>
+		  <i class="align-middle" data-feather="bar-chart-2"></i>
+		  <span class="align-middle">Detail Honor Assessment</span>
+		</a>
+	  </li>
+	  <li class="sidebar-item">
+		<a
+		  class="sidebar-link"
+		  href="https://euis.ulbi.ac.id/hris-dev/app/honor/assessment-validasi.html"
+		>
+		  <i class="align-middle" data-feather="check"></i>
+		  <span class="align-middle"
+			>Dashboard Validasi Honor Assessment</span
+		  >
+		</a>
+	  </li>
+	  <li class="sidebar-header">
+		Honor Dosen Wali
+		<i
+		  class="fa fa-fw fa-check-square-o"
+		  style="
+			background-color: #ffffff;
+			color: rgb(255, 0, 0);
+			padding: 2px 5px;
+			border-radius: 5px;
+			font-size: 6.5px;
+		  "
+		  >On Progress</i
+		>
+	  </li>
+	  <li class="sidebar-item">
+		<a
+		  class="sidebar-link"
+		  href="https://euis.ulbi.ac.id/hris-dev/app/dosenwali/dosenwali-master.html"
+		>
+		  <i class="align-middle" data-feather="activity"></i>
+		  <span class="align-middle">Detail Honor Dosen Wali</span>
+		</a>
+	  </li>
+	  <li class="sidebar-header">
+		Honor Prodi
+		<i
+		  class="fa fa-fw fa-check-square-o"
+		  style="
+			background-color: #ffffff;
+			color: rgb(255, 0, 0);
+			padding: 2px 5px;
+			border-radius: 5px;
+			font-size: 6.5px;
+		  "
+		  >On Progress</i
+		>
+	  </li>
+	  <li class="sidebar-item">
+		<a
+		  class="sidebar-link"
+		  href="https://euis.ulbi.ac.id/hris-dev/app/prodi/primbon-prodi.html"
+		>
+		  <i class="align-middle" data-feather="box"></i>
+		  <span class="align-middle">Detail Primbon Honor Prodi</span>
+		</a>
+	  </li>
+	  <li class="sidebar-item">
+		<a
+		  class="sidebar-link"
+		  href="https://euis.ulbi.ac.id/hris-dev/app/prodi/prodi-master.html"
+		>
+		  <i class="align-middle" data-feather="briefcase"></i>
+		  <span class="align-middle">Detail Honor Prodi</span>
+		</a>
+	  </li>
+  `;
+
+  tableHeader.innerHTML = `
+  <thead>
+				<tr
+				  style="text-align: center; vertical-align: middle"
+				>
+				  <th
+					class="name-email-header"
+					style="background-color: #2b343d"
+				  >
+					Nama
+				  </th>
+				  <!-- <th id="posisiTh">Jabatan</th> -->
+				  <th id="statusTh">Gaji Pokok</th>
+				  <th id="tanggalTh">Keluarga</th>
+				  <th id="DurasiTh">Pangan</th>
+				  <th id="pctDurasiTh">KPI</th>
+				  <th id="keteranganTh">Keahlian</th>
+				  <!-- <th id="linkDokumenTh">Struktural</th> -->
+
+				  <th id="linkDokumenTh">FGS/Struktural</th>
+				  <th id="linkDokumenTh">Transport</th>
+				  <th id="linkDokumenTh">Kehadiran</th>
+				  <th id="linkDokumenTh">Rapel Gaji</th>
+
+				  <th id="linkDokumenTh">Kopkar</th>
+				  <th id="linkDokumenTh">Bank Jabar</th>
+				  <th id="linkDokumenTh">Arisan</th>
+				  <th id="linkDokumenTh">BPJS TK</th>
+				  <th id="linkDokumenTh">BAUK</th>
+				  <th id="linkDokumenTh">Lain - lain</th>
+				  <th id="linkDokumenTh">PPH</th>
+				  <th id="linkDokumenTh">Total Gaji</th>
+				  <th id="linkDokumenTh">Total Gaji Bersih</th>
+				  <th id="linkDokumenTh">Total Gaji Potongan</th>
+				  <th id="linkDokumenTh">Action</th>
+				</tr>
+			  </thead>
+`;
+} else {
+  sidebarMenus.innerHTML = `
+   <li class="sidebar-header">
+		Penggajian
+		<i
+		  class="fa fa-fw fa-check-square-o"
+		  style="
+			background-color: #ffffff;
+			color: rgb(255, 0, 0);
+			padding: 2px 5px;
+			border-radius: 5px;
+			font-size: 6.5px;
+		  "
+		  >Testing BETA</i
+		>
+	  </li>
+	  <li class="sidebar-item">
+		<a
+		  class="sidebar-link"
+		  href="https://euis.ulbi.ac.id/hris-dev/app/history-slip-gaji-bulanan.html"
+		>
+		  <i class="align-middle" data-feather="file-text"></i>
+		  <span class="align-middle">History Slip Gaji Perbulan</span>
+		</a>
+	  </li>
+  `;
+
+  tableHeader.innerHTML = `
+  <thead>
+				<tr
+				  style="text-align: center; vertical-align: middle"
+				>
+				  <th
+					class="name-email-header"
+					style="background-color: #2b343d"
+				  >
+					Nama
+				  </th>
+				  <th id="linkDokumenTh">Total Gaji</th>
+				  <th id="linkDokumenTh">Total Gaji Bersih</th>
+				  <th id="linkDokumenTh">Total Gaji Potongan</th>
+				  <th id="linkDokumenTh">Action</th>
+				</tr>
+			  </thead>
+`;
+}
+
 // Fungsi umum untuk penyortiran tabel
 function sortTable(table, columnIndex, ascending) {
-	const tableBody = table.querySelector('tbody');
-	const rows = Array.from(tableBody.querySelectorAll('tr'));
+  const tableBody = table.querySelector("tbody");
+  const rows = Array.from(tableBody.querySelectorAll("tr"));
 
-	rows.sort((a, b) => {
-		const aValue = a.cells[columnIndex].textContent;
-		const bValue = b.cells[columnIndex].textContent;
-		return aValue.localeCompare(bValue, undefined, { numeric: true });
-	});
+  rows.sort((a, b) => {
+    const aValue = a.cells[columnIndex].textContent;
+    const bValue = b.cells[columnIndex].textContent;
+    return aValue.localeCompare(bValue, undefined, { numeric: true });
+  });
 
-	if (!ascending) {
-		rows.reverse();
-	}
+  if (!ascending) {
+    rows.reverse();
+  }
 
-	tableBody.innerHTML = '';
+  tableBody.innerHTML = "";
 
-	rows.forEach(row => tableBody.appendChild(row));
+  rows.forEach((row) => tableBody.appendChild(row));
 }
 
 // Pasang pendengar acara untuk tombol penyortiran di setiap kolom
-const table = document.getElementById('example');
-const headerCells = table.querySelectorAll('th');
+const table = document.getElementById("example");
+const headerCells = table.querySelectorAll("th");
 
 headerCells.forEach((cell, index) => {
-	// Tambahkan tombol arah penyortiran pada setiap header kolom
-	cell.innerHTML += '<span class="sort-icon">&#8595;</span>';
+  // Tambahkan tombol arah penyortiran pada setiap header kolom
+  cell.innerHTML += '<span class="sort-icon">&#8595;</span>';
 
-	let ascending = true;
+  let ascending = true;
 
-	cell.addEventListener('click', () => {
-		// Toggle arah penyortiran saat tombol di klik
-		ascending = !ascending;
+  cell.addEventListener("click", () => {
+    // Toggle arah penyortiran saat tombol di klik
+    ascending = !ascending;
 
-		// Hapus tombol arah penyortiran pada semua kolom
-		headerCells.forEach(headerCell => {
-			headerCell.querySelector('.sort-icon').textContent = '';
-		});
+    // Hapus tombol arah penyortiran pada semua kolom
+    headerCells.forEach((headerCell) => {
+      headerCell.querySelector(".sort-icon").textContent = "";
+    });
 
-		// Tambahkan tombol arah penyortiran pada kolom yang di-klik
-		cell.querySelector('.sort-icon').textContent = ascending ? '↓' : '↑';
+    // Tambahkan tombol arah penyortiran pada kolom yang di-klik
+    cell.querySelector(".sort-icon").textContent = ascending ? "↓" : "↑";
 
-		// Panggil fungsi penyortiran dengan arah yang sesuai
-		sortTable(table, index, ascending);
-	});
+    // Panggil fungsi penyortiran dengan arah yang sesuai
+    sortTable(table, index, ascending);
+  });
 });
 
 // Untuk Sorting Data berdasarkan tanggal (start date to end date)
 // Fungsi untuk memeriksa apakah suatu tanggal berada dalam rentang tertentu
 function isDateInRange(date, startDate, endDate) {
-	return date >= startDate && date <= endDate;
+  return date >= startDate && date <= endDate;
 }
 
 // Fungsi untuk menerapkan filter berdasarkan rentang tanggal
 function applyDateFilter(table, startDate, endDate) {
-	const tableBody = table.querySelector('tbody');
-	const rows = Array.from(tableBody.querySelectorAll('tr'));
+  const tableBody = table.querySelector("tbody");
+  const rows = Array.from(tableBody.querySelectorAll("tr"));
 
-	rows.forEach((row) => {
-		const dateCell = row.cells[3]; // Kolom ke-3 (Tangal)
-		const dateValue = new Date(dateCell.textContent);
-		row.style.display = isDateInRange(dateValue, startDate, endDate) ? '' : 'none';
-	});
+  rows.forEach((row) => {
+    const dateCell = row.cells[3]; // Kolom ke-3 (Tangal)
+    const dateValue = new Date(dateCell.textContent);
+    row.style.display = isDateInRange(dateValue, startDate, endDate)
+      ? ""
+      : "none";
+  });
 }
 
 // Pasang pendengar acara untuk tombol filter berdasarkan tanggal
-const startDateInput = document.getElementById('startDate');
-const endDateInput = document.getElementById('endDate');
+const startDateInput = document.getElementById("startDate");
+const endDateInput = document.getElementById("endDate");
 // const filterButton = document.getElementById('filterButton');
 
 // filterButton.addEventListener('click', () => {
